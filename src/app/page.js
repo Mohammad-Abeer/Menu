09e74cea44e9a712ec9d94da192d0d0574f3f0ce
Mohammad-Abeer <s.mohammadabeer@gmail.com>
2024-08-27 21:52:33 +0500

    code modifications

--- src/app/page.js
+++ src/app/page.js
@@ -1,108 +1,12 @@
-import styles from "./page.module.css";
-import Landing from "@/pages/Landing";
-import Menu from "@/pages/Menu";
-import Menu2 from "@/pages/Menu2";
-import { Box, ChakraProvider, Container } from "@chakra-ui/react";
+import styles from "./page.module.scss";
+import { ChakraProvider } from "@chakra-ui/react";
+import RenderMenus from "./RenderMenus";
 
 export default function Home() {
   return (
     <ChakraProvider>
       <main className={styles.main}>
-        <Box width={["90%"]} margin={"auto"}>
-          <div className={styles.landing}>
-            <Landing />
-          </div>
-          <Menu />
-          <Menu2 />
-        </Box>
-
-        {/* <Box className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </Box>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-          
-        </a>
-      </div> */}
+        <RenderMenus />
       </main>
     </ChakraProvider>
   );
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,52 +1,55 @@
 import React from 'react'
-import Logo from "@/images/Logo.png"
 import Image from 'next/image'
-import { Box, Grid, GridItem, Stack, StackDivider, Text, VStack } from '@chakra-ui/react'
-import { itemCost25, itemCost30, itemCost35, itemCost40 } from '../contants/lovs'
+import Logo from "@/images/Logo.png"
+import { Box, Grid, GridItem, StackDivider, Text, VStack } from '@chakra-ui/react'
+import { itemCost25, itemCost30, itemCost35, itemCost40 } from '@/contants/lovs'
 
 /* pound sign = £ */
 
 const Menu = () => {
-
-
   const Section = ({ title, category }) => {
     return (
       <>
-        <Box style={{ color: "#fff" }}>
+        <Box style={{ color: "#fff" }} width={["95%", "85%", "75%", "60%"]} margin={"auto"}>
           {/* Title */}
-          <Text fontSize={"30px"} letterSpacing={"5px"} my={"4rem"}>{title}</Text>
-          
-          <Grid templateColumns={[`repeat(1, 1fr)`, `repeat(2, 1fr)`]}  columnGap={40} rowGap={5}>
+          <Text textAlign={'center'} fontSize={["18px", "22px", "26px", "30px"]}  letterSpacing={"5px"} my={"4rem"}>{title}</Text>
+
+          <Grid templateColumns={[`repeat(1, 1fr)`, `repeat(2, 1fr)`]} columnGap={40} rowGap={5}>
 
             {/* category.map() function - Loops through every index inside category array. */}
             {category?.map((item, index) => (
               <GridItem key={index}>
-                <Text fontSize={"25px"}>
+                <Text fontSize={["16px", "18px", "22px", , "25px"]}>
                   {item.name}
-                  </Text>
-                </GridItem>
+                </Text>
+              </GridItem>
             ))}
           </Grid>
         </Box>
 
         {/* Silver line below every section */}
-        <StackDivider mt={"40px"} border={"6px solid #31363F"} className='divider'/>
+        <StackDivider mt={"40px"} border={"6px solid #31363F"} className='divider' />
       </>
     )
   }
 
-
   return (
-    <Box my={"10rem"} style={{  position: "relative" }}>
+    <Box my={"10rem"} style={{ position: "relative" }}>
 
       <Box display={["none", "none", "block"]} style={{ position: "absolute", left: "10px", }}>
         <Image alt='logo' src={Logo} style={{ width: "8rem", height: "auto" }} />
       </Box>
 
-
       <VStack style={{ textAlign: "center" }}>
         {/* Title */}
-        <Text color={'white'} fontSize={"84px"} fontWeight={"800"} letterSpacing={'1px'} textTransform={'uppercase'}>
+        <Text
+          textAlign={'center'}
+          color={'white'}
+          fontSize={["54px", "64px", "74px", "84px"]}
+          fontWeight={"800"}
+          letterSpacing={'1px'}
+          textTransform={'uppercase'}
+        >
           Shisha
         </Text>
 
